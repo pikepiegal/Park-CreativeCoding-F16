800af5c6657837469c1f5a278d35a1cd1da7ef37
Daniel Park <dysp137@gmail.com>
2016-09-30 19:10:14 -0400

    hw upload

---
+++ Assignment 9.29/sketch.js
@@ -0,0 +1,18 @@
+var offset = 2.5;
+var spacing = 10;
+
+function setup(){
+  createCanvas(1200,600);
+}
+
+function draw(){
+  for (var i = 0; i < 10; i+=1) {
+  	for (var j = 10; j < 38; j+=1){
+      line(i, j*spacing, width/4-offset*j, j*spacing);
+      line(width/4-offset*j, j*spacing, width/3, height/8 + j*spacing);
+      line(width/3, height/8 + j*spacing, width*3/5, j*spacing - height/7);
+      line(width*3/5, j*spacing - height/7, width*4/5-offset*j, j*spacing);
+      line(width*4/5-offset*j, j*spacing, width, j*spacing);
+  	}
+  }
+}
